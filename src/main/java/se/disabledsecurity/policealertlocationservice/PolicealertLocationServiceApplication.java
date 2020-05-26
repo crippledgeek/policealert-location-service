f74b486540a5c4b6ac5ba9e188dae01321c3836f
Mattias Carlsson <mattias.carlsson01@gmail.com>
2020-05-27 01:40:14 +0200

    Initial commit

---
+++ src/main/java/se/disabledsecurity/policealertlocationservice/PolicealertLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package se.disabledsecurity.policealertlocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicealertLocationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicealertLocationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/disabledsecurity/policealertlocationservice/PolicealertLocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.disabledsecurity.policealertlocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicealertLocationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
